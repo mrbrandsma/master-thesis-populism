23d1c4fcf5a3ebb7de8a9f05f24fd583a3e385c3
Marije Brandsma <45396743+mrbrandsma@users.noreply.github.com>
2023-02-17 14:16:17 +0100

    Uploading coalition_large.csv

--- code/retrieve_tweets.py
+++ code/retrieve_tweets.py
@@ -1,14 +1,16 @@
-import snscrape.modules.twitter as sntwitter
-import pandas as pd
-import json
-
 from helper import get_tweets
 
+# For date:
+    # Last election: 2021-03-26
+    # Before-last election: 2015-03-15
+
+# Change settings here
 party_data_path = '../data/party_data.json'
-outfile = '../data/last_election_large.csv'
-# If no limit wanted: use 1
+outfile = '../data/coalition_large.csv'
+# If no limit wanted: use 0
 limit_per_party = 0
-since = '2021-03-26'
+since = '2015-03-15'
 until = '2023-02-17'
 
+# Collect tweets
 get_tweets(party_data_path, limit_per_party, outfile, since, until)
